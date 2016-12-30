89d933761c31ff53ab03210563a4a6cbaf49af7b
TineeOne <dongchen2015@gmail.com>
2016-12-30 11:08:22 +0800

    add ex1-17 of LPTHW

---
+++ ex11.py
@@ -0,0 +1,21 @@
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weigh?",
+weight = raw_input ()
+
+print "So, you're %r old, %r tall and %r heavy." % (
+    age, height, weight)
+
+
+print "How many books do you have?",
+books = raw_input()
+print "How many pens do you have?",
+pens = raw_input()
+print "How much sugar do you have?",
+sugar = raw_input()
+
+print "So you have %r books, %r pens, %r sugar." % (
+    books, pens, sugar)
+
